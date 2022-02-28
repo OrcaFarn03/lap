b42cf82809bf985942b5657363b31d8176143b1d
OrcaFarn03 <61849565+OrcaFarn03@users.noreply.github.com>
2022-02-28 16:43:35 +0100

    Add files via upload

---
+++ JW-nicerdicer/html_gerüst.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>MainPage</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Stylesheets -->
+        <link rel="stylesheet" type="text/css" href="CSS/mainStyle.css">
+        <!-- JavaScript -->
+        <script type="text/javascript" src="JS/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="JS/main.js"></script>
+    </head>
+    <body>
+
+
+
+    </body>
+</html>
