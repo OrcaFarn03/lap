112a0ccdc6a58e4c7ab9ec616d378ba23e824fa1
OrcaFarn03 <61849565+OrcaFarn03@users.noreply.github.com>
2022-03-01 15:02:59 +0100

    Delete VorbereitungJR directory

--- VorbereitungJR/Formular/Login.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-    if ($_POST['benutzername'] == "Joshua" AND $_POST['password'] == "Test1234*")
-    {
-    //$_SESSION['benutzername'] = $_POST['benutzername'];
-    $_SESSION['eingeloggt'] = true;
-    echo "<b>Einloggen erfolgreich</b>";
-    }
-?>
